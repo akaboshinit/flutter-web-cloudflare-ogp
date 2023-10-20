83e855e5bf510b7745c0ac2e54a9a16ca02666a5
akaboshinit <akaboshinit@gmail.com>
2023-10-20 16:49:02 +0900

    FlutterWeb metaタグ編集用の設定

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'flutter-web-cloudflare-ogp',
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
